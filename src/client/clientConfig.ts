5c0fd0f2ab407cd9038db88b1ceba8a26687febc
eplaza <eplaza@liveperson.com>
2020-06-08 13:46:09 +0200

    30s timeout

--- src/client/clientConfig.ts
+++ src/client/clientConfig.ts
@@ -58,7 +58,7 @@ export const defaultConfig: Required<DefaultConfig> = {
   gwCsdsServiceName: 'faasGW',
   uiCsdsServiceName: 'faasUI',
   apiVersion: '1',
-  timeout: 65000, // ms
+  timeout: 30000, // ms
   protocol: PROTOCOL.HTTPS,
   getLambdasUri: 'api/account/%s/lambdas/',
   invokeUuidUri: 'api/account/%s/lambdas/%s/invoke',
